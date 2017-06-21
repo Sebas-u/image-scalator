10dff94ea88b6718d8d12b87dfdf655385edf227
Sebastián <sebastian.urraco@gmail.com>
2017-06-21 10:26:43 +0200

    Create index
    Index where select images to upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title></title>
+
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="icon" href="images/favicon.png">
+</head>
+
+<body>
+  <form action="test.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+  </form>
+</body>
+
+</html>
+
+
