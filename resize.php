a34145a1eefec8b4e3a49886c63810ec908b21f4
Sebastián <sebastian.urraco@gmail.com>
2017-06-21 10:53:20 +0200

    Add new variables to function call

--- resize.php
+++ resize.php
@@ -1,14 +1,18 @@
 <?php
-//Movemos el archivo temporal a la carpeta deseada para poder tratar la imagen, guardando la imagen en $img
-$tmp_name =  $_FILES['fileToUpload']['tmp_name'];
-$name = $_FILES['fileToUpload']['name'];
-move_uploaded_file($tmp_name,"images/".$name);
-$img = "images/".$name;
+if(isset($_POST)){
+  include_once "functions/image-scalator.php";
+  
+  //Movemos el archivo temporal a la carpeta deseada para poder tratar la imagen, guardando la imagen en $img
+  $tmp_name =  $_FILES['fileToUpload']['tmp_name'];
+  $name = $_FILES['fileToUpload']['name'];
+  move_uploaded_file($tmp_name,$ruta.$name);
+  $img = $ruta.$name;
 
-include_once "functions/image-scalator.php";
+  $new_width  = $_POST['width'];
+  $new_height = $_POST['height'];
+  $download = $_POST['download'];
 
-$new_width  = $_POST['width'];
-$new_height = $_POST['height'];
-resizeImg($img,$new_width,$new_height);
+  resizeImg($img,$new_width,$new_height,$download);
 
+}
 header("location:index.php");
