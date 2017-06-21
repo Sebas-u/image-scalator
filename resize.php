89b65c3bc2b87c3ed2da8f8076d58a62f4918719
Sebastián <sebastian.urraco@gmail.com>
2017-06-21 11:47:44 +0200

    Fixing resize.php

--- resize.php
+++ resize.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_POST)){
   include_once "functions/image-scalator.php";
-  
+  $ruta = "test_images/";
   //Movemos el archivo temporal a la carpeta deseada para poder tratar la imagen, guardando la imagen en $img
   $tmp_name =  $_FILES['fileToUpload']['tmp_name'];
   $name = $_FILES['fileToUpload']['name'];
@@ -11,7 +11,6 @@ if(isset($_POST)){
   $new_width  = $_POST['width'];
   $new_height = $_POST['height'];
   $download = isset($_POST['download'])?1:0;
-  resizeImg($img,$new_width,$new_height,$download);
-
+  resizeImg($img,$new_width,$new_height,$ruta,$download);
 }
 header("location:index.php");
