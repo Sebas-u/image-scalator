987c9d3825065667ce91e92fd061871c525215e6
Sebastián <sebastian.urraco@gmail.com>
2017-06-21 11:48:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <title></title>
-
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="icon" href="images/favicon.png">
 </head>
 
 <body>
@@ -27,6 +22,8 @@
 </body>
 
 </html>
+
+
 
 
 
