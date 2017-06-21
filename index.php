c7a1c068c38c927d4b92dcdacd85de971166e8fa
Sebastián <sebastian.urraco@gmail.com>
2017-06-21 10:46:20 +0200

    Add width, height and download option to the form

--- index.php
+++ index.php
@@ -12,10 +12,17 @@
 </head>
 
 <body>
-  <form action="test.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
+  <form action="resize.php" method="post" enctype="multipart/form-data">
+    <label for="fileToUpload">Select image to upload:</label>
     <input type="file" name="fileToUpload" id="fileToUpload">
+    <label for="width">Select new width:</label>
+    <input type="number" min="0" name="width" id="width">
+    <label for="height">Select new height:</label>
+    <input type="number" min="0" name="height" id="height">
+    <label for="download">Download?</label>
+    <input type="checkbox" name="checkbox" value="1">
     <input type="submit" value="Upload Image" name="submit">
+    
   </form>
 </body>
 
